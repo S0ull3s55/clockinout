a1e1ea5742bb1cef0283f5cd412899bef530d886
S0ull3s55 <847azj@gmail.com>
2025-08-24 16:21:06 +0100

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,22 +1,5 @@
-import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
-import { Platform } from 'react-native';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
-export const supabase = typeof window !== 'undefined' 
-  ? createClient(supabaseUrl, supabaseAnonKey, {
-      auth: {
-        storage: Platform.OS === 'web' ? undefined : AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: Platform.OS === 'web',
-      },
-    })
+export const supabase = typeof window !== 'undefined'
+  ? createClient(process.env.EXPO_PUBLIC_SUPABASE_URL || '', process.env.EXPO_PUBLIC_ANON_KEY || '')
   : null;
