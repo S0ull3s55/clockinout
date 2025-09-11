1bffa9d7a391457e1e03cda595866ccbf059eef6
S0ull3s55 <847azj@gmail.com>
2025-09-11 14:24:44 +0100

    Fix Metro bundler minifier configuration error

--- metro.config.js
+++ metro.config.js
@@ -11,12 +11,4 @@ config.resolver.sourceExts.push('sql', 'db');
 // Ensure proper handling of symlinks and node_modules
 config.resolver.unstable_enableSymlinks = true;
 
-// Configure transformer for better compatibility
-config.transformer.minifierConfig = {
-  keep_fnames: true,
-  mangle: {
-    keep_fnames: true,
-  },
-};
-
 module.exports = config;
